ca10c714916f817c70d2c9e45eb28e9cdf6d65ea
John Filo <johnfilo@layerm.com.au>
2016-06-07 14:29:38 +1000

    VPN Creation via CD Pipeline

---
+++ provision.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+SOLACE_IP=192.168.190.140:8080
+CRED=admin:admin
+
+CREATE_VPN="\
+<rpc semp-version=\"soltr/7_1_1\">
+    <create>
+        <message-vpn>
+            <vpn-name>Solace-CICD</vpn-name>
+        </message-vpn>
+    </create>
+</rpc>\
+"
+START_VPN="\
+<rpc semp-version=\"soltr/7_1_1\">
+    <message-vpn>
+        <vpn-name>Solace-CICD</vpn-name>
+        <no>
+            <shutdown></shutdown>
+        </no>
+    </message-vpn>
+</rpc>\
+"
+
+echo "${CREATE_VPN}" | curl -d @- -u "${CRED}" http://${SOLACE_IP}/SEMP
+echo "${START_VPN}" | curl -d @- -u "${CRED}" http://${SOLACE_IP}/SEMP
